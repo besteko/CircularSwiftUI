6d5f95f8568615a4df0dc33dc03e1651d349086d
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-08-26 12:03:58 +0300

    Initial Commit

---
+++ CircularSwiftUI/CircularSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularSwiftUIApp.swift
+//  CircularSwiftUI
+//
+//  Created by Beste Kocaoglu on 26.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CircularSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
