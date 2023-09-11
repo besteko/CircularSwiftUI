9ecad2d97295bf14de03e30eb11d77747d85837a
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-09-11 10:46:00 +0300

    first commit

--- CircularSwiftUI/ContentView.swift
+++ CircularSwiftUI/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State var firstCirclePercentange : Double = 0
+    @State var secondCirclePercentange : Double = 0
+    @State var thirdCirclePercentange : Double = 0
+     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            
+            Circular(lineWidth: 40, backgroundColor: Color.blue.opacity(0.2), foregroundColor: Color.blue, percentage: firstCirclePercentange)
+                .frame(width: 350, height: 350)
+                .onTapGesture {
+                    if self.firstCirclePercentange == 0 {
+                        self.firstCirclePercentange = 50
+                    } else {
+                        self.firstCirclePercentange = 0
+                    }
+                }
+            
+            Circular(lineWidth: 40, backgroundColor: Color.red.opacity(0.2), foregroundColor: Color.red, percentage: secondCirclePercentange)
+                .frame(width: 250, height: 250)
+                .onTapGesture {
+                    if self.secondCirclePercentange == 0 {
+                        self.secondCirclePercentange = 75
+                    } else {
+                        self.secondCirclePercentange = 0
+                    }
+                }
+            
+            Circular(lineWidth: 40, backgroundColor: Color.green.opacity(0.2), foregroundColor: Color.green, percentage: thirdCirclePercentange)
+                .frame(width: 150, height: 150)
+                .onTapGesture {
+                    if self.thirdCirclePercentange == 0 {
+                        self.thirdCirclePercentange = 100
+                    } else {
+                        self.thirdCirclePercentange = 0
+                    }
+                }
+            
+            
         }
-        .padding()
+        
     }
 }
 
